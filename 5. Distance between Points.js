e36f15629390590df3ff1a561c4daa920fbda59a
Miroslav Tsarev <mtzarev@gmail.com>
2024-08-15 17:10:10 +0300

    Add files via upload

---
+++ 5. Distance between Points.js
@@ -0,0 +1,10 @@
+function distanceBetweenPoints(x1, y1, x2, y2) {
+
+    let k1 = Math.abs(x1 - x2);
+    let k2 = Math.abs(y1 - y2);
+    let distance = Math.sqrt(Math.pow(k1,2) + Math.pow(k2,2));
+    
+    console.log(distance);
+}
+distanceBetweenPoints(2.34, 15.66, -13.55, -2.9985
+);
